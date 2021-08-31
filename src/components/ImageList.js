9da69fd8e10f9a95782362846eedce643f0d336c
gianluca galota <gianluca.galota@gmail.com>
2021-08-31 16:48:36 +0100

    images grid span dynamic

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import ImageCard from './ImageCard';
 
 // Styles
 import './ImageList.scss';
 
 const ImageList = (props) => {
   console.log(props.images)
-  const images = props.images.map(({ id, urls, description }) => {
-    return <img key={id} src={urls.regular} alt={description} />
+
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />
   })
 
   return (
