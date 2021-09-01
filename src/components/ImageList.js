33f8c505f67679787cfaea8a9babf7a13193c8dc
gianluca galota <gianluca.galota@gmail.com>
2021-09-01 12:21:31 +0100

    fixed title

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -5,7 +5,6 @@ import ImageCard from './ImageCard';
 import './ImageList.scss';
 
 const ImageList = (props) => {
-  console.log(props.images)
 
   const images = props.images.map((image) => {
     return <ImageCard key={image.id} image={image} />
